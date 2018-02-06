1667872aeadee71cb39f4e30179d259d0974ad8b
Chris Rithmyxay <crithmyxay@gmail.com>
2018-02-06 14:42:30 -0500

    taking break

--- documents/src/App.js
+++ documents/src/App.js
@@ -4,46 +4,47 @@ import './App.css';
 import LeftPane from './left-pane'
 import RightPane from './right-pane'
 
-const movies = [
-  {
-    title: 'The Fast and the Furious',
-    description: 'Fast Cars',
-  },
-  {
-    title: '2 Fast 2 Furious',
-    description: 'Fast Cars in Miami',
-  },
-  {
-    title: 'The Fast and The Furious: Tokyo Drift',
-    description: 'Fast cars go sideways',
-  },
-  {
-    title: 'Fast & Furious',
-    description: 'Back to fast cars',
-  },
-  {
-    title: 'Fast Five',
-    description: 'Fast cars driving away',
-  },
-  {
-    title: 'Fast & Furious 6',
-    description: 'Fast cars helping save world',
-  },
-  {
-    title: 'Furious 7',
-    description: 'Fast cars jumps out of buildings',
-  },
-  {
-    title: 'The Fate of the Furious',
-    description: 'Boss of fast cars becomes bad guy',
-  }
-];
+
 
 class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      index: 0
+      index: 0,
+      movies: [
+        {
+          title: 'The Fast and the Furious',
+          description: 'Fast Cars',
+        },
+        {
+          title: '2 Fast 2 Furious',
+          description: 'Fast Cars in Miami',
+        },
+        {
+          title: 'The Fast and The Furious: Tokyo Drift',
+          description: 'Fast cars go sideways',
+        },
+        {
+          title: 'Fast & Furious',
+          description: 'Back to fast cars',
+        },
+        {
+          title: 'Fast Five',
+          description: 'Fast cars driving away',
+        },
+        {
+          title: 'Fast & Furious 6',
+          description: 'Fast cars helping save world',
+        },
+        {
+          title: 'Furious 7',
+          description: 'Fast cars jumps out of buildings',
+        },
+        {
+          title: 'The Fate of the Furious',
+          description: 'Boss of fast cars becomes bad guy',
+        }
+      ]
     }
   }
 
@@ -59,8 +60,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <div className="main">
-          <LeftPane movies={movies} handleClick={this._setIndex}/>
-          <RightPane movie={movies[this.state.index]} />
+          <LeftPane movies={this.state.movies} handleClick={this._setIndex}/>
+          <RightPane movie={this.state.movies[this.state.index]} handleChange={this._setDescription}/>
         </div>
       </div>
     );
@@ -73,6 +74,22 @@ class App extends Component {
       console.log(num); 
     });
   }
+
+  _setDescription = (desc) => {
+    this.setState({
+      movies: this.state.movies.map((movie, i) => {
+        if (i === this.state.index) {
+          return {
+            title: movie.title,
+            description: desc
+          }
+        } else {
+          return movie
+        }
+      })
+    })
+  }
+
 }
 
 export default App;
